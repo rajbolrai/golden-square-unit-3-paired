7a6134c4bb564e08208d7285bff5ad4462c5bde6
rajbolrai <rajbolrai@hotmail.com>
2025-06-04 14:18:04 +0100

    added final tests

--- lib/design_function.py
+++ lib/design_function.py
@@ -1,6 +1,4 @@
 def include_to_do(string):
-    if len(string) == 0:
-        return False
     if "#TODO" in string:
         return True
     return False
--- tests/test_design_function.py
+++ tests/test_design_function.py
@@ -11,3 +11,11 @@ def test_given_TODO__in_string_output_true():
 def test_given_string_with_TODO_will_output_true():
     result = include_to_do("hello #TODO")
     assert result == True
+    
+def test_given_string_without_TODO_will_output_false():
+    result = include_to_do("hello")
+    assert result == False
+    
+def test_given_with_TODO_at_start_output_true():
+    result = include_to_do("#TODO hello")
+    assert result == True
